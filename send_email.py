f51c26171c9f97066011db3ab0e73104bae1468d
MX <fuhaojun@52tt.com>
2024-06-03 11:34:12 +0800

    feat: 密码 放在 系统环境变量中 open ~/.zshrc

--- send_email.py
+++ send_email.py
@@ -1,15 +1,17 @@
-import smtplib, ssl
+import smtplib, ssl, os
 
 
-def send_email(receiver, message):
+def send_email():
     host = 'smtp.gmail.com'
     port = 465
 
     username = 'markfu1996@gmail.com'
-    password = 'xxx'
+    # 和 js 不一样 不是单独 .env 文件 写在系统 环境变量中  open ~/.zshrc
+    password = os.getenv('PASSWORD_EMAIL')
 
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
+send_email()
